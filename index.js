d44fd550646a80d2792a5dcb7601faec9f576fae
Paula Yas <paulayas1108@gmail.com>
2022-02-12 21:41:42 -0300

    string,number e boolean

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var algumaCoisa = ""
+var nomeEstudante = "Ana";
+ nomeEstudante = "ana"
+
+ var aprovada = true;
+
+ var hub = "Belo Horizonte";
+
+ var turma = 1
+
+ var turmaHub = "turma 1 Belo horizonte"
+
+ var salario = 3500.70
+
+ typeof "ana";
+
+console.log(typeof(salario));
