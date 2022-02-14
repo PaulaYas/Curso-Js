090d1dff146b429de42f1547bc583a29fcdbb566
Paula Yas <paulayas1108@gmail.com>
2022-02-13 23:15:16 -0300

    Estruturas condicionais

---
+++ EstrurasCondicionais.js
@@ -0,0 +1,23 @@
+var notaDoDesafio = 90;
+if (notaDoDesafio >= 100) {
+    console.log("Aprovado");
+} else {
+    console.log("Nao aprovado");
+}
+
+var entendiTudo = true;
+if (entendiTudo) {
+    console.log("Seguimos em frente com a aula"); 
+} else { 
+    console.log("Rever conteudo")};
+
+
+var luzSemaforo = "vermelho";
+
+if (luzSemaforo === "verde"){
+    console.log("Siga em frente");
+} else if (luzSemaforo === "amarela"){
+    console.log("Atencao! sinal ficara vermelho");
+} else {
+    console.log("sinal verelho! Pare!");
+}
