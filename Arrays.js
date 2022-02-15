3e3a8d0bc52c1d814f0377e008c3ba6da6631b83
Paula Yas <paulayas1108@gmail.com>
2022-02-15 20:19:45 -0300

    Arrys

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+var listaDeNomes = ["Paula", "Rebeca", "Luana", "Bruna", "Vitoria"];
+listaDeNomes[0];
+listaDeNomes.push("Debora");
+listaDeNomes.length;
+ var tamanhoDaListaDeNomes = listaDeNomes;
+
+ var coresDaBandeira = ["Verde", "Amarelo", "Branco"];
+
+ coresDaBandeira.length;
+
+ coresDaBandeira[0];
+ coresDaBandeira[2];
+ coresDaBandeira[1];
+coresDaBandeira[coresDaBandeira.length - 1];
+console.log(coresDaBandeira[coresDaBandeira.length - 1]);
